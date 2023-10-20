b528f491730ff1a3453190b73e53d909c9fefc94
Nico Lerchl <nico@lerchl.one>
2023-10-20 10:07:05 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ccc2023-02
+
+Extend `Solver` for each level and implement its solve method. Then change the level that gets created in `Runner` and adjust the sub levels if neccessary.
---
+++ src/Level1.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Level1 extends Solver {
+
+	Level1() {
+		super(1);
+	}
+
+	@Override
+	protected List<String> solve(List<String> input) {
+		throw new UnsupportedOperationException("Unimplemented method 'solve'");
+	}
+}
